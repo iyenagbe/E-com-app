e4a9604da38661c9e4597b2d7b6e249439049eb9
iyenagbe <iyenagberaymond@gmail.com>
2025-06-15 18:10:45 +0100

    working

--- e-com-frontend/src/Components/BestSeller.jsx
+++ e-com-frontend/src/Components/BestSeller.jsx
@@ -2,6 +2,8 @@ import React, { useEffect } from 'react'
 import { useContext, useState } from 'react';
 import { ShopContext } from '../Context/ShopContext';
 import Title from './Title';
+import ProductItem from './ProductItem';
+
 
 const BestSeller = () => {
 
@@ -24,7 +26,14 @@ const BestSeller = () => {
             Inventore maiores laborum dolores dolor ratione! Beatae assumenda aliquam tempora,
             maxime hic unde omnis rem eius placeat. Possimus,
             </p>
+        </div>
 
+        <div className='grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-5 gap-y-6'>
+           {
+            bestSeller.map((item,index) => (
+                <ProductItem key={index} id={item._id} name={item.name} image={item.image} price={item.price} />
+            ))
+           }
         </div>
       
     </div>
