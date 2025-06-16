b6de197f501ca3c2988251315af30a79ccb09864
iyenagbe <iyenagberaymond@gmail.com>
2025-06-16 15:22:24 +0100

    working

--- e-com-frontend/src/Components/Footer.jsx
+++ e-com-frontend/src/Components/Footer.jsx
@@ -1,9 +1,46 @@
 import React from 'react'
+import { assets } from '../assets/assets'
 
 const Footer = () => {
   return (
     <div>
-      
+      <div className='flex flex-col sm:grid grid-cols-[3fr_1fr_1fr] gap-14 my-10 mt-40 text-sm'>
+
+        <div>
+            <img src={assets.logo} className='mb-5 w-32' alt="" />
+            <p className='text-gray-500 w-full sm:w-3/4 '>
+                Rayjock Store is your one-stop shop for the latest fashion trends and accessories. 
+                We offer a wide range of products to suit every style and budget, with a focus on 
+                quality and customer satisfaction.
+            </p>
+        </div>
+
+            <div>
+                <p className='text-xl font-medium mb-5 '>COMPANY</p>
+                <ul className='flex flex-col gap-3 text-gray-500'>
+                   <li>Home</li>
+                   <li>About</li>
+                   <li>Delivery</li>
+                   <li>Privacy policy</li>
+
+                </ul>
+            </div>
+
+            <div>
+                <p className='text-xl font-medium mb-5'>GET IN TOUCH</p>
+                <ul className='flex flex-col gap-3 text-gray-500'>
+                    <li>+234 821264733</li>
+                    <li>info@rayjockstore.com</li>
+
+                </ul>
+            </div>
+      </div>
+
+      <div>
+        <hr />
+        <p className='py-5 text-sm text-center'>Copyright 2025 @ rayjockstore.com - All Right Reserved. </p>
+
+      </div>
     </div>
   )
 }
