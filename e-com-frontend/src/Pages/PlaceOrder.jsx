d89d06219c5931232ba57555da2ff61d81307a1b
iyenagbe <iyenagberaymond@gmail.com>
2025-07-21 00:13:05 +0100

    working

--- e-com-frontend/src/Pages/PlaceOrder.jsx
+++ e-com-frontend/src/Pages/PlaceOrder.jsx
@@ -1,9 +1,81 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Title from '../Components/Title'
+import CartTotal from '../Components/CartTotal'
+
+
+
 
 const PlaceOrder = () => {
+const [method, setMethod] = useState('cod');
+
+
   return (
-    <div>
-      
+    <div className='flex flex-col sm:flex-row justify-between gap-3 pt-5 sm:pt-14 min-h-[80vh border-t]'>
+          {/* left side of place order  */}
+      <div className='flex flex-col gap-4 w-full sm:max-w-[480px]'>
+        <div className='text-xl sm:text-2xl my-3'>
+          <Title text1={'DELIVERY'} text2={'DETAILS'}/>
+        </div>
+
+              {/* input filed  */}
+        <div className='flex gap-3'>
+          <input className='border border-gray rounded py-1.5 px-3.5 w-full' type="text" placeholder='First name' />
+          <input className='border border-gray rounded py-1.5 px-3.5 w-full' type="text" placeholder='Last name' />
+        </div>
+
+          <input className='border border-gray rounded py-1.5 px-3.5 w-full' type="email" placeholder='Email address' />
+         <input className='border border-gray rounded py-1.5 px-3.5 w-full' type="text" placeholder='Home address' />
+
+         <div className='flex gap-3'>
+          <input className='border border-gray rounded py-1.5 px-3.5 w-full' type="text" placeholder='City' />
+          <input className='border border-gray rounded py-1.5 px-3.5 w-full' type="text" placeholder='State' />
+        </div>
+
+        <div className='flex gap-3'>
+          <input className='border border-gray rounded py-1.5 px-3.5 w-full' type="number" placeholder='Zip code' />
+          <input className='border border-gray rounded py-1.5 px-3.5 w-full' type="text" placeholder='Country' />
+        </div>
+
+        <input className='border border-gray rounded py-1.5 px-3.5 w-full' type="number" placeholder='Phone' />
+      </div> 
+
+              {/* right side      */}
+           <div className='mt-8'>
+            <div className='mt-8 min-w-80'>
+              <CartTotal />
+            </div>
+            
+            <div className='mt-12'>
+              <Title text1={'PAYMENT'} text2={'METHOD'}/>
+                  {/* payment method options  */}
+
+              <div className='flex gap-3 flex-col lg:flex-row'>
+
+                <div onClick={() => setMethod('Mastercard')} className='flex items-center gap-3 border p-2 px-3 cursor-pointer'>
+                <p className={`min-w-3.5 h-3.5 border rounded-full ${method === 'Mastercard' ? 'bg-green-500' : ''}`}></p>
+                <img
+                  src="https://upload.wikimedia.org/wikipedia/commons/0/04/Mastercard-logo.png"
+                  className='h-5 mx-4'
+                  alt="Mastercard"
+                />
+              </div>
+
+                <div onClick={() => setMethod('Visa')} className='flex items-center gap-3 border p-2 px-3 cursor-pointer'>
+                <p className={`min-w-3.5 h-3.5 border rounded-full ${method === 'Visa' ? 'bg-green-500' : ''}`}></p>
+                <img
+                  src="https://upload.wikimedia.org/wikipedia/commons/4/41/Visa_Logo.png"
+                  className='h-5 mx-4'
+                  alt="Visa"
+                />
+              </div>
+
+              <div onClick={() => setMethod('cod')} className='flex items-center gap-3 border p-2 px-3 cursor-pointer'>
+                <p className={`min-w-3.5 h-3.5 border rounded-full ${method === 'cod' ? 'bg-green-500' : ''}`}></p>
+                <p className='text-gray-500 text-sm font-medium mx-4 '>CASH ON DELIVERY</p>
+              </div>
+            </div>
+            </div>
+          </div>      
     </div>
   )
 }
