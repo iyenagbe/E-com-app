f4e2973889d1383305cc703d3b48cda3f3784dfa
iyenagbe <iyenagberaymond@gmail.com>
2025-07-21 13:13:51 +0100

    working

--- e-com-frontend/src/Pages/Orders.jsx
+++ e-com-frontend/src/Pages/Orders.jsx
@@ -1,8 +1,23 @@
 import React from 'react'
+import { useContext } from 'react'
+import {ShopContext} from '../Context/ShopContext'
+import Title from '../Components/Title'
 
 const Orders = () => {
+
+  const {products, currency} = useContext(ShopContext);
+
+
+
   return (
-    <div>
+    <div className='border-t pt-16'>
+      <div className='text-2xl'>
+        <Title  text1={'MY'} text2={'ORDER'}/>
+      </div>
+
+      <div >
+
+      </div>
       
     </div>
   )
