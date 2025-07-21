d50f072d0e11f127527fb5a411c5333b0a5f261b
iyenagbe <iyenagberaymond@gmail.com>
2025-07-21 15:23:13 +0100

    working

--- e-com-frontend/src/Pages/Orders.jsx
+++ e-com-frontend/src/Pages/Orders.jsx
@@ -37,11 +37,12 @@ const Orders = () => {
       </div>
 
       {/* Right-aligned status */}
-      <div className='md:w-1/4 flex md:justify-start'>
+      <div className='md:w-1/4 flex md:justify-between'>
         <div className='flex items-center gap-2'>
           <p className='min-w-2 h-2 rounded-full bg-green-500'></p>
           <p className='text-sm md:text-base'>Ready to Ship</p>
         </div>
+          <button className='border px-4 py-2 text-sm font-medium rounded-sm'>Track Order</button>
       </div>
     </div>
   ))
